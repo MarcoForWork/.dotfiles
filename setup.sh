ab8b809652d2abe6bec0fc72661598c1250a6932
Marco <hoang.le2160.4work@gmail.com>
2024-02-06 13:44:55 +0700

    add setup script | hopefully i'll work

---
+++ setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Change to the directory where the scripts are located
+cd ~/.dotfiles/.local/bin || exit
+
+#Script
+./autores
+./vital-apps.sh
+./paru-install.sh
+./install-suckless-applications.sh
+./ssh-server.sh
+./make-directory.sh
+./install-lazyvim.sh
+./install-doom-emacs.sh
+./install-oh-my-zsh.sh
+
+#Back to HOME
+cd
